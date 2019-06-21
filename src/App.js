dad4c7703834a7cfe5fc4f9bb3b040997af90641
hemanth <hemanthkumar.m16@iiits.in>
2019-06-21 16:22:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './style.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <Game />
+      </div>
     </div>
   );
+}
+class Game extends React.Component{
+  render() {
+    return(
+      <Board />
+    );
+  }
+
+}
+
+class Board extends React.Component {
+  render() {
+    return(
+      <div className="board">
+        <div className="board-row">
+          <Square value={1}/>
+          <Square value={2}/>
+          <Square value={3}/>
+        </div>
+        <div className="board-row">
+          <Square value={4}/>
+          <Square value={5}/>
+          <Square value={6}/>
+        </div>
+        <div className="board-row">
+          <Square value={7}/>
+          <Square value={8}/>
+          <Square value={9}/>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+class Square extends React.Component {
+  render() {
+    return(
+        <button className="square">{this.props.value} </button>
+      );
+  }
+
 }
 
 export default App;
